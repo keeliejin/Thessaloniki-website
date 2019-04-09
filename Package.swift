80a5e0b9ae5a21785dff7a1751691925ba63ee28
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-04-09 00:19:40 -0600

    Updated dependencies. (#7)

--- Package.swift
+++ Package.swift
@@ -1,12 +1,15 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "Generator",
+    platforms: [
+        .macOS(.v10_13)
+    ],
     dependencies: [
-        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .exact(Version(0, 12, 0))),
-        .package(url: "https://github.com/SDGGiesbrecht/SDGWeb", .exact(Version(0, 0, 2)))
+        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .exact(Version(0, 16, 0))),
+        .package(url: "https://github.com/SDGGiesbrecht/SDGWeb", .exact(Version(0, 0, 3)))
         ],
     targets: [
         .target(
